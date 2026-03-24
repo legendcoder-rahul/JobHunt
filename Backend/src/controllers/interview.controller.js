1bca8af46165536e140711772aff9575c6116a2a
Rahul <rahul251035@gmail.com>
2026-03-24 17:08:59 +0530

    model err

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -1,6 +1,6 @@
 const pdfParse = require("pdf-parse")
-const { generateInterviewReport, generateResumePdf } = require("../services/ai.service")
-const interviewReportModel = require("../models/interviewReport.model")
+const { generateInterviewReport, generateResumePdf } = require("../services/ai.service.js")
+const interviewReportModel = require("../models/interviewReport.model.js")
 
 
 /**
